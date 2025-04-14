328cafcd6b2eac6d32dd41d77fe904c26545ddc1
mrlexcoder <mrlexcoder@gmail.com>
2025-04-15 05:01:25 +0530

    working on portfolio web

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -337,39 +337,7 @@ export const DATA = {
         },
       ],
     },
-    // {
-    //   title: "HackDavis",
-    //   dates: "January 20th - 21st, 2018",
-    //   location: "Davis, California",
-    //   description:
-    //     "Developed a mobile application which allocates a daily carbon emission allowance to users to move towards a sustainable environment.",
-    //   image:
-    //     "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-davis.png",
-    //   win: "Best Data Hack",
-    //   mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2018/white.svg",
-    //   links: [
-    //     {
-    //       title: "Devpost",
-    //       icon: <Icons.globe className="h-4 w-4" />,
-    //       href: "https://devpost.com/software/my6footprint",
-    //     },
-    //     {
-    //       title: "ML",
-    //       icon: <Icons.github className="h-4 w-4" />,
-    //       href: "https://github.com/Wallet6/my6footprint-machine-learning",
-    //     },
-    //     {
-    //       title: "iOS",
-    //       icon: <Icons.github className="h-4 w-4" />,
-    //       href: "https://github.com/Wallet6/CarbonWallet",
-    //     },
-    //     {
-    //       title: "Server",
-    //       icon: <Icons.github className="h-4 w-4" />,
-    //       href: "https://github.com/Wallet6/wallet6-server",
-    //     },
-    //   ],
-    // },
+   
     {
       title: "ETH Waterloo",
       dates: "October 13th - 15th, 2017",
