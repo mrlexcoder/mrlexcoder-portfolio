685b17d34a46f1183ccd452e3a520012b6280a7f
mrlexcoder <mrlexcoder@gmail.com>
2025-04-15 05:45:38 +0530

    working on portfolio web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -164,7 +164,7 @@ export default function Page() {
                   I like building things
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-  Since my university days, I've been building projects. Here are some of my latest ones.
+  Since my university days, I&apos;ve been building projects. Here are some of my latest ones.
 </p>
 
               </div>
